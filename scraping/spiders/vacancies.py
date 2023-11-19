36bb75fbe284236f5ab2ca23b1c754d8fddff60a
Inna Isareva <inna8110302@gmail.com>
2023-11-19 20:40:58 +0200

    split logic into different functions

--- scraping/spiders/vacancies.py
+++ scraping/spiders/vacancies.py
@@ -28,23 +28,62 @@ class VacanciesSpider(scrapy.Spider):
     def parse_vacancy(self, response: Response) -> Generator[dict, None, None]:
         sel = Selector(text=response.body)
 
-        company = response.css(".job-details--title::text").get()
-        salary = response.css(".public-salary-item::text").get()
+        company = self.get_company(response)
+        salary = self.get_salary(response)
+        english_level = self.get_english_level(sel)
+        experience_year = self.get_experience_year(response)
+        domen = self.get_domen(sel)
+        work_type, company_type, test_available = self.get_additional_info(sel)
+        views = self.get_views(response)
+        applications = self.get_applications(response)
+        publication_date = self.get_publication_date(response)
+
+        yield {
+            "title": response.css("h1::text").get().strip(),
+            "company": company,
+            "salary": salary,
+            "english_level": english_level,
+            "experience_year": experience_year,
+            "domen": domen,
+            "work_type": work_type,
+            "company_type": company_type,
+            "test_available": test_available,
+            "views": views,
+            "applications": applications,
+            'publication_date': publication_date,
+        }
+
+    @staticmethod
+    def get_company(response: Response) -> Optional[str]:
+        return response.css(".job-details--title::text").get().strip() if response.css(
+            ".job-details--title::text") else None
+
+    @staticmethod
+    def get_salary(response: Response) -> Optional[str]:
+        return response.css(".public-salary-item::text").get().strip() if response.css(
+            ".public-salary-item::text") else None
+
+    @staticmethod
+    def get_english_level(sel: Selector) -> Optional[str]:
         english_level = sel.xpath('//div[contains(text(), "Англійська:")]/text()').get()
-        experience_year = int(
-            response.css(
-                ".job-additional-info--body li:last-child div::text"
-            )
-            .get()
-            .split()[0]
-            .replace("Без", "0")
-        )
+        return english_level.strip().split(": ")[1] if english_level else None
+
+    @staticmethod
+    def get_experience_year(response: Response) -> Optional[int]:
+        exp_text = response.css(".job-additional-info--body li:last-child div::text").get()
+        if exp_text:
+            experience_year = int(exp_text.split()[0].replace("Без", "0"))
+            return experience_year
+        return None
+
+    @staticmethod
+    def get_domen(sel: Selector) -> Optional[str]:
         domen = sel.xpath('//div[contains(text(), "Домен:")]/text()').get()
+        return domen.strip().split(": ")[1] if domen else None
 
-        work_type = None
-        company_type = None
-        test_available = None
-
+    @staticmethod
+    def get_additional_info(sel: Selector) -> tuple:
+        work_type, company_type, test_available = None, None, None
         items = sel.css('li.job-additional-info--item')
         for item in items:
             icon_class = item.css('span::attr(class)').get()
@@ -59,44 +98,29 @@ class VacanciesSpider(scrapy.Spider):
             elif 'bi bi-pencil-square' in icon_class and text == 'Є тестове завдання':
                 test_available = 1
 
-        views = int(response.css("p.text-muted").re_first(r"(\d+) відгук"))
-        applications = int(response.css("p.text-muted").re_first(r"(\d+) відгук"))
+        return work_type, company_type, test_available
 
-        yield {
-            "title": response.css("h1::text").get().strip(),
-            "company": company.strip() if company else None,
-            "salary": salary.strip() if salary else None,
-            "english_level": english_level.strip().split(": ")[1] if english_level else None,
-            "experience_year": experience_year if experience_year else None,
-            "domen": domen.strip().split(": ")[1] if domen else None,
-            "work_type": work_type,
-            "company_type": company_type,
-            "test_available": test_available,
-            "views": views,
-            "applications": applications,
-            'publication_date': self.get_publication_date(response),
-        }
+    @staticmethod
+    def get_views(response: Response) -> int:
+        views_text = response.css("p.text-muted").re_first(r"(\d+) відгук")
+        return int(views_text) if views_text else 0
+
+    @staticmethod
+    def get_applications(response: Response) -> int:
+        apps_text = response.css("p.text-muted").re_first(r"(\d+) відгук")
+        return int(apps_text) if apps_text else 0
 
     def get_publication_date(self, response: Response) -> str:
         date_text = response.css("p.text-muted").extract_first()
-        publication_date = date_text.split('Вакансія опублікована')[-1].strip()
-        publication_date = publication_date.split("<br>")[0].strip()
+        publication_date = date_text.split('Вакансія опублікована')[-1].strip().split("<br>")[0].strip()
         return self.format_data(publication_date)
 
     @staticmethod
     def format_data(publication_date: str):
         months_dict = {
-            'січня': 'January',
-            'лютого': 'February',
-            'березня': 'March',
-            'квітня': 'April',
-            'травня': 'May',
+            'січня': 'January', 'лютого': 'February', 'березня': 'March', 'квітня': 'April', 'травня': 'May',
             'червня': 'June',
-            'липня': 'July',
-            'серпня': 'August',
-            'вересня': 'September',
-            'жовтня': 'October',
-            'листопада': 'November',
+            'липня': 'July', 'серпня': 'August', 'вересня': 'September', 'жовтня': 'October', 'листопада': 'November',
             'грудня': 'December'
         }
         for k, v in months_dict.items():
@@ -105,5 +129,3 @@ class VacanciesSpider(scrapy.Spider):
         date_obj = datetime.strptime(publication_date, "%d %B %Y")
         date_only = date_obj.date()
         return date_only
-
-
