bfae9806bff8a21473362461c1abbf6d4824701a
Inna Isareva <inna8110302@gmail.com>
2023-11-19 19:46:34 +0200

    extracted company and salary

--- scraping/spiders/vacancies.py
+++ scraping/spiders/vacancies.py
@@ -25,9 +25,20 @@ class VacanciesSpider(scrapy.Spider):
 
     def parse_vacancy(self, response: Response) -> Generator[dict, None, None]:
         sel = Selector(text=response.body)
+
+        company = response.css(".job-details--title::text").get()
+        salary = response.css(".public-salary-item::text").get()
         english_level = sel.xpath('//div[contains(text(), "Англійська:")]/text()').get()
-        experience = sel.xpath('//div[contains(text(), "роки досвіду")]/text()').get()
+        experience_year = int(
+            response.css(
+                ".job-additional-info--body li:last-child div::text"
+            )
+            .get()
+            .split()[0]
+            .replace("Без", "0")
+        )
         domen = sel.xpath('//div[contains(text(), "Домен:")]/text()').get()
+
         work_type = None
         company_type = None
         test_available = None
@@ -44,14 +55,16 @@ class VacanciesSpider(scrapy.Spider):
             elif 'bi bi-exclude' in icon_class:
                 company_type = text.strip() if text else None
             elif 'bi bi-pencil-square' in icon_class and text == 'Є тестове завдання':
-                test_available = '1'
+                test_available = 1
 
         yield {
             "title": response.css("h1::text").get().strip(),
+            "company": company.strip() if company else None,
+            "salary": salary.strip() if salary else None,
             "english_level": english_level.strip().split(": ")[1] if english_level else None,
-            "experience_year": experience.strip().split()[0] if experience else None,
+            "experience_year": experience_year if experience_year else None,
+            "domen": domen.strip().split(": ")[1] if domen else None,
             "work_type": work_type,
             "company_type": company_type,
             "test_available": test_available,
-            "domen": domen.strip().split(": ")[1] if domen else None,
         }
