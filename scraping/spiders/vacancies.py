a92d6bedb83ad07aa6800ff5e38acce1fa8f57bd
Inna Isareva <inna8110302@gmail.com>
2023-11-19 19:31:17 +0200

    extracted work_type, company_type, test_available, domen

--- scraping/spiders/vacancies.py
+++ scraping/spiders/vacancies.py
@@ -27,9 +27,31 @@ class VacanciesSpider(scrapy.Spider):
         sel = Selector(text=response.body)
         english_level = sel.xpath('//div[contains(text(), "Англійська:")]/text()').get()
         experience = sel.xpath('//div[contains(text(), "роки досвіду")]/text()').get()
+        domen = sel.xpath('//div[contains(text(), "Домен:")]/text()').get()
+        work_type = None
+        company_type = None
+        test_available = None
+
+        items = sel.css('li.job-additional-info--item')
+        for item in items:
+            icon_class = item.css('span::attr(class)').get()
+            text = item.css('div.job-additional-info--item-text::text').get()
+
+            if 'bi bi-building' in icon_class:
+                work_type = text.strip() if text else None
+            elif 'bi bi-basket3-fill' in icon_class:
+                company_type = text.strip() if text else None
+            elif 'bi bi-exclude' in icon_class:
+                company_type = text.strip() if text else None
+            elif 'bi bi-pencil-square' in icon_class and text == 'Є тестове завдання':
+                test_available = '1'
 
         yield {
             "title": response.css("h1::text").get().strip(),
             "english_level": english_level.strip().split(": ")[1] if english_level else None,
             "experience_year": experience.strip().split()[0] if experience else None,
+            "work_type": work_type,
+            "company_type": company_type,
+            "test_available": test_available,
+            "domen": domen.strip().split(": ")[1] if domen else None,
         }
